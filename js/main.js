5f3dbfe5cbabce9a26d076a62e5a799d3d84e8d2
Martin <martin.sachin@gmail.com>
2021-04-23 14:15:58 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let stage = 0
+function sayNope(){
+    $('#inner').html("<p>Nope. </p>");
+}
+
+$('#main_input').submit((e) => {
+    e.preventDefault();
+
+    let field = $('#input_field');
+    let submitted = field.val();
+
+    switch (stage) {
+        case 0:
+            if (submitted === "2421") {
+                $('#image').attr('src', 'img/hlav2.jpg');
+                $('#text').text("Součet:");
+                field.val('');
+
+                stage = 1;
+            } else {
+                sayNope();
+            }
+            break;
+        case 1:
+            if (submitted === "3089") {
+                $('#image').remove();
+                $('#text').text("50°06'08.0\"N 14°21'57.4\"E");
+                field.remove();
+            } else {
+                sayNope();
+            }
+            break;
+    }
+
+
+    console.log();
+});
