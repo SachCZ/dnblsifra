de47fcc6e6ef77664a98910c15982ad0c838c848
Martin <martin.sachin@gmail.com>
2021-04-23 16:12:53 +0200

    Added stage 1.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let stage = 0
 function sayNope(){
-    $('#inner').html("<p>Nope. </p>");
+    $('#wrapper').html("<p>Nope. </p>");
 }
 
 $('#main_input').submit((e) => {
@@ -8,23 +8,39 @@ $('#main_input').submit((e) => {
 
     let field = $('#input_field');
     let submitted = field.val();
+    let image = $('#image');
 
     switch (stage) {
         case 0:
             if (submitted === "2421") {
-                $('#image').attr('src', 'img/hlav2.jpg');
-                $('#text').text("Součet:");
+                image.hide();
+                $('#text').text("Jak musí Áňa vykrmit Matěje, aby se pod ním 3m dlouhé prkno " +
+                    "o šířce 0.5m a výšce 40mm prohnulo o 18mm. Modul pružnosti prkna je 11GPa. Uvažujte prkno " +
+                    "podepřené jako prostý nosník a tíhové zrychlení zaokrouhlené na celá čísla. " +
+                    "Vlastní tíhu prkna zanedbejte. " +
+                    "Kód je přesný výsledek v kilogramech napsaný bez desetinné čárky.");
                 field.val('');
-
                 stage = 1;
             } else {
                 sayNope();
             }
             break;
         case 1:
+            if (submitted === "11264") {
+                image.show();
+                image.attr('src', 'img/hlav2.jpg');
+                $('#text').text("Součet:");
+                field.val('');
+                stage = 2;
+            } else {
+                sayNope();
+            }
+            break;
+        case 2:
             if (submitted === "3089") {
-                $('#image').remove();
+                image.remove();
                 $('#text').text("50°06'08.0\"N 14°21'57.4\"E");
+                $('#clickable').remove();
                 field.remove();
             } else {
                 sayNope();
